aa61c1d214094e3547dae54e3a578058f8fa18b6
Michał Młynarczyk <michal.mlynarczyk@benefitsystems.pl>
2026-02-04 22:22:35 +0100

    droplet

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ resource "digitalocean_project" "main" {
     purpose = "Development"
     description = "my second project for mastering terraforms"
 }
+
+resource "digitalocean_droplet" "main" {
+    name= "main droplet"
+    image= "ubuntu-20-04-x64"
+    size="s-1vcpu-1gb"
+    region="nyc3"  
+}
